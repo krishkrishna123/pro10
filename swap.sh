9aa02f4aa06f5623a91bc2656016995357b0ad97
krishkrishna123 <krishna4925a@gmail.com>
2026-09-11 12:12:23 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -2,10 +2,29 @@
 
 # =====================================
 # Swap Space Creation Script
-# Student Name:
-# Roll Number:
+# Student Name:KRISHNAKUMAR.R
+# Roll Number:1U24IT058
 # =====================================
 
-# Write your commands below
+# Create a 1 GB swap file
+sudo fallocate -l 1G /swapfile
+
+# Set correct permissions
+sudo chmod 600 /swapfile
+
+# Format the file as swap space
+sudo mkswap /swapfile
+
+# Enable the swap space
+sudo swapon /swapfile
+
+# Make swap permanent after reboot
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Display swap status
+sudo swapon --show
+
+# Display memory and swap usage
+free -h
 
 
